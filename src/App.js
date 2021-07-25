33ab8a4bcc2214086419bb20bd0541fa374bca01
Alejandro Perdomo <diegoleandro-29@hotmail.com>
2021-07-25 15:41:33 -0500

    Creando el formulario

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, {Fragment} from 'react';
+import Formulario from './components/Formulario';
 
 
 function App() {
   return (
-    <h1>Administrador de pacientes</h1>
+    <Fragment>
+      <h1>Administrador de pacientes</h1>
+
+      <div className="container">
+        <div className="row">
+          <div className="one-half column">
+              <Formulario />
+          </div>
+          <div className="one-half column">
+              2
+          </div>
+        </div>
+      </div>
+    </Fragment>
+    
   );
 }
 
---
+++ src/components/Formulario.js
@@ -0,0 +1,52 @@
+import React, {Fragment} from 'react';
+const Formulario = () => {
+    return (
+        <Fragment>
+            <h2>Crear cita</h2>
+            <form>
+                <label>Nombre Mascota</label>
+                <input
+                    type="text"
+                    name="mascota"
+                    className="u-full-width"
+                    placeholder="Nombre Mascota"
+                />
+
+                <label>Nombre Dueño</label>
+                <input
+                    type="text"
+                    name="propietario"
+                    className="u-full-width"
+                    placeholder="Nombre Dueño"
+                />
+
+                <label>Fecha</label>
+                <input
+                    type="date"
+                    name="fecha"
+                    className="u-full-width"
+                />
+
+                <label>Hora</label>
+                <input
+                    type="time"
+                    name="hora"
+                    className="u-full-width"
+                />
+
+                <label>Sintomas</label>
+                <textarea
+                    className="u-full-width"
+                    name="sintomas"
+                ></textarea>
+
+                <button
+                    type="submit"
+                    className="u-full-width button-primary"
+                >Agregar cita</button>
+            </form>
+        </Fragment>
+    );
+}
+ 
+export default Formulario;
