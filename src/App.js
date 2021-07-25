27411bef1a7dab464e485f7ec18a90c8a6e61fb4
Alejandro Perdomo <diegoleandro-29@hotmail.com>
2021-07-25 17:56:48 -0500

    Implementando Local Storage al proyecto

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
-import React, {Fragment, useState} from 'react';
+import React, {Fragment, useState, useEffect} from 'react';
 import Formulario from './components/Formulario';
 import Cita from './components/Cita';
 
 
 function App() {
 
+  //Descargar del citas de localStorage
+  let citasIniciales = JSON.parse(localStorage.getItem('citas'));
+  if(!citasIniciales){
+    citasIniciales = [];
+  }
   //Arreglo de citas
 
-  const [citas, guardarCitas] = useState([]);
+  const [citas, guardarCitas] = useState([citasIniciales]);
+
+  //UseEffect para realizar ciertas operaciones cuando el state cambia
+  useEffect(()=>{
+    if(citasIniciales){
+      localStorage.setItem('citas', JSON.stringify(citas));
+    }else{
+      localStorage.setItem('citas', JSON.stringify([]));
+    }
+  }, [citas]);
 
   //Funcion que tome las citas actuales y agregue la nueva
 
